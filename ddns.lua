48d29adfa927294f0f8e6382731b2e9891f31124
Felix Braun <hazzl@falix.de>
2023-10-20 21:33:24 +0200

    added ipv6 support

--- ddns.lua
+++ ddns.lua
@@ -3,12 +3,15 @@ local DOMAIN = "MY_DOMAIN"
 local CUSTOMERNO = 12345
 local APIKEY = "APIKEY_AS_CREATED_IN_CCP"
 local APIPASSWD = "APIPASSWD_AS_CREATED_IN_CCP"
+local IP6_DEVICE = "/64 of the device that runs the script"
+local IP6_SERVER = "/64 of the server"
 ---------------------------------------------------------
 local cjson = require "luci.jsonc"
 local https = require "ssl.https"
 local ltn12 = require "ltn12"
 local socket = require "socket"
 local http = socket.http
+local string = require "string"
 
 local function send_command(coms,ids)
 	local postbod
@@ -38,17 +41,24 @@ local function clear()
 	return t
 end
 
-local com, id, response, ip4, modified
+local com, id, response, ip4, ipv6, modified
 
 response = {}
 http.request{url = "http://v4.ident.me", sink = ltn12.sink.table(response)}
 ip4=table.concat(response)
 if ip4 == "" then -- something went terribly wrong. Give up.
+	print ("no network")
 	return
 end
 
+response = {}
+http.request{url = "http://v6.ident.me", sink = ltn12.sink.table(response)}
+ipv6 = table.concat(response)
+ipv6,_ = (string.gsub(ipv6, IP6_DEVICE, IP6_SERVER))
+
 response = socket.dns.toip(DOMAIN)
 if ip4 == response then 
+	print ("address not changed")
 	return
 end
 local logfile=io.open("ddns.log","a")
@@ -69,6 +79,9 @@ modified = false
 for _,v in pairs (response.responsedata.dnsrecords) do
 	if v.type == "A" and v.hostname ~= "mail" and v.destination ~= ip4 then
 		v.destination = ip4
+		modified = true
+	elseif v.type == "AAAA" and v.destination ~= ipv6 then
+		v.destination = ipv6
 		modified = true
 	end
 end
